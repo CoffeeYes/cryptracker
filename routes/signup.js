d90f2a4fcf8285eb9fad29eda001f9aaf95d0273
CoffeeYes <culinecro@gmail.com>
2018-02-02 13:21:03 +0100

    setting up database checks for signup form

--- routes/signup.js
+++ routes/signup.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var functions = require('../public/javascripts/functions.js');
 var mClient = require('mongodb').MongoClient;
+var bcrypt = require('bcrypt');
 
 //file containing api keys included
 var api_keys = require('../bin/api_keys.js')
@@ -26,17 +27,38 @@ router.post('/',function(req,res,next) {
 
   //TODO check email against regex
 
-  mClient.connect(mongo_url,function(error,database) {
+  mClient.connect(api_keys.mongo_url,function(error,database) {
     if(error)throw error;
+    //check if username is taken
+    database.collection(api_keys.mongo_collection_name).find({Username: req.body.username}).toArray(function(error,data) {
+      if(error)throw error;
+      if(data != '') {
+        return res.render('signup',{error: "username is already in use"})
+      }
+      else {
+        //check if email is taken
+        database.collection(api_keys.mongo_collection_name).find({Email: req.body.email}).toArray(function(error,data) {
+          if(data != '') {
+            return res.render('signup',{error: "email is already in use"})
+          }
+          else {
 
-    var user_info = {
-      Username: req.body.username,
-      Email: req.body.email,
-      //need to hash this
-      Password: req.body.password1,
-    }
+            var user_info = {
+              Username: req.body.username,
+              Email: req.body.email
+            }
 
-    database.collection(mongo_collection_name).insertOne(user_info)
+            //asynchronously hash the password and push user_info to database on callback
+            bcrypt.hash(req.body.password1,10,function(error,hash) {
+              if(error)throw error;
+              user_info.Password = hash;
+              database.collection(api_keys.mongo_collection_name).insertOne(user_info)
+              return res.redirect('/')
+            })
+          }
+        })
+      }
+    })
   })
 });
 

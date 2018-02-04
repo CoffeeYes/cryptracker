81f3f797ccfd7131fb915241fbb0a56ff30da759
CoffeeYes <culinecro@gmail.com>
2018-02-04 12:23:14 +0100

    added route and function for logout

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var signup = require('./routes/signup');
 var login = require('./routes/login');
+var logout = require('./routes/logout');
 
 
 var app = express();
@@ -52,6 +53,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/signup',signup);
 app.use('/login',login);
+app.use('/logout',logout);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

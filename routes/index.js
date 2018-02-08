05a4715ba7b10c062964d3dca3461c5dd711331e
CoffeeYes <culinecro@gmail.com>
2018-02-08 14:03:40 +0100

    added basic fetch and render of tickers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mClient = require('mongodb').MongoClient;
+var api_keys = require('../bin/api_keys.js');
+var ObjectId = require('mongodb').ObjectID;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (!req.session.userId) {
+    res.render('index');
+  }
+  else {
+    mClient.connect(api_keys.mongo_url,function(error,database) {
+      if(error)throw error;
+      database.collection(api_keys.mongo_collection_name).find({_id: ObjectId(req.session.userId)}).toArray(function(error,data) {
+        if(error)throw error;
+        res.render('index',{ticker_arr: data[0].cryptos})
+      })
+    })
+  }
 });
 
 module.exports = router;

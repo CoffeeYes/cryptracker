4e769f5b99df6cc4abb99d4f519efaa3a95bbc20
CoffeeYes <culinecro@gmail.com>
2018-02-12 15:07:57 +0100

    changed data to get from database instead of calling bitfinex public api

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ var api_keys = require('../bin/api_keys.js');
 var ObjectId = require('mongodb').ObjectID;
 var functions = require('../public/javascripts/functions.js')
 var request = require('request');
+var ticker_table = require('../public/javascripts/ticker_table.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //if user is logged in
+  //if user is not logged in
   if (!req.session.userId) {
     res.render('index');
   }
@@ -17,27 +18,20 @@ router.get('/', function(req, res, next) {
       if(error)throw error;
       database.collection(api_keys.mongo_collection_name).find({_id: ObjectId(req.session.userId)}).toArray(function(error,data) {
         if(error)throw error;
-        //use promises to get api data for each ticker and render to index once complete
-        var tickers = data[0].cryptos;
-        var promises = [];
 
-        //get api data for each ticker
-        for(var i = 0; i < tickers.length; i++) {
-          promises.push(functions.get_api_data_2(tickers[i].exchange,tickers[i].currency));
-        }
+        //get the users saved crypto currencies
+        var user_cryptos = data[0].cryptos;
 
-        request.get("https://api.bitfinex.com/v1/symbols",function(error,response,body) {
-          console.log(body)
-        })
+        //lookup the users cryptos and pull current ticker bid data from database, then render index with pulled data
+        database.collection(api_keys.db_crypto.collection_name).find({_id: ObjectId(api_keys.db_crypto.id)}).toArray(function(error,data) {
 
-        //once all promises have resolved, comput current value and render to index
-        Promise.all(promises).then(function(result) {
-          for(var i = 0;i < tickers.length; i++) {
-            var temp = JSON.parse(result[i]);
-            var current_ask = temp.ask;
-            tickers[i].Cvalue = (parseFloat(current_ask) * parseFloat(tickers[i].volume) ).toFixed(2)
+          for(var i = 0; i < user_cryptos.length;i++) {
+            var current_crypto = user_cryptos[i].currency;
+            var current_exchange = user_cryptos[i].exchange;
+            var current_ticker = ticker_table.table[current_exchange][current_crypto];
+            user_cryptos[i].Cvalue = data[0].Bitfinex[current_ticker]
           }
-          res.render('index',{ticker_arr: tickers})
+          return res.render('index',{ticker_arr: user_cryptos})
         })
       })
     })

8ef2369625cda898eff41101d015c9d9cd467a83
CoffeeYes <culinecro@gmail.com>
2018-02-14 14:04:09 +0100

    added id's to tickers

--- routes/add_ticker.js
+++ routes/add_ticker.js
@@ -8,11 +8,21 @@ router.post('/',function(req,res) {
   mClient.connect(api_keys.mongo_url,function(error,database) {
     if(error)throw error;
     var push_data = req.body;
-    //calculate original value
-    push_data.Ovalue = (parseInt(push_data.volume) * parseFloat(push_data.buyIn)).toFixed(2);
-    database.collection(api_keys.mongo_collection_name).update({_id: ObjectId(req.session.userId)},{$push : {cryptos: push_data}});
-    database.close();
-    res.redirect('/')
+    database.collection(api_keys.mongo_collection_name).find({_id: ObjectId(req.session.userId)}).toArray(function(error,data) {
+      if(error)throw error;
+      //get id for ticker
+      push_data.id = data[0].ticker_id;
+      //calculate original value
+      push_data.Ovalue = (parseInt(push_data.volume) * parseFloat(push_data.buyIn)).toFixed(2);
+
+      //increment id value on db
+      database.collection(api_keys.mongo_collection_name).update({_id: ObjectId(req.session.userId)},{$inc : {ticker_id : 1}});
+
+      //push info to database
+      database.collection(api_keys.mongo_collection_name).update({_id: ObjectId(req.session.userId)},{$push : {cryptos: push_data}});
+      database.close();
+      res.redirect('/')
+    })
   })
 })
 

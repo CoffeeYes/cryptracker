c24167722fd1d464e08b51064d2effc44f69628b
CoffeeYes <culinecro@gmail.com>
2018-02-24 12:07:48 +0100

    added button to close add-ticker form

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -33,9 +33,13 @@ var exchange_list = {
 }
 
 $(document).ready(function() {
-  console.log("jquery ready")
+
   $('.add-button').click(function() {
     $('.opaque-background').css('display','flex');
+  })
+
+  $('.close-form').click(function() {
+    $('.opaque-background').css('display','none');
   })
 
   //load up the initial dropdown values
